8cfcdb32dc9fb480fd2920807a5899d82270660e
Yuta Saotome <42575295+utiuti12345@users.noreply.github.com>
2020-11-29 21:22:20 +0900

    update github actions workflow

--- src/config/GoogleSignInParams.js
+++ src/config/GoogleSignInParams.js
@@ -1,6 +1,6 @@
 const GOOGLE_SIGN_IN_PARAMS = {
     client_id: process.env.REACT_APP_CLIENT_ID,
-    ux_mode: 'redirect',  // popupはブラウザーでブロックされる
+    ux_mode: 'popup',  // popupはブラウザーでブロックされる
 };
 
 export default GOOGLE_SIGN_IN_PARAMS;
